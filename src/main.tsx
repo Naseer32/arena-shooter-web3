21ce3a3b05c0e1c5680f530410752d3b6a33fa27
Naseer32 <76082276+Naseer32@users.noreply.github.com>
2026-09-03 11:04:35 +0100

    Wrap App component with WagmiProvider and QueryClientProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { WagmiProvider } from 'wagmi'
 import App from './ui/App'
 import './ui/styles.css'
+import { queryClient, wagmiConfig } from './web3'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </WagmiProvider>
+  </React.StrictMode>,
 )
