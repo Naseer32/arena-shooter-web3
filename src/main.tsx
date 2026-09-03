8cc3713e6ee0146af27c58b6585bbdb3b53248f5
Naseer32 <76082276+Naseer32@users.noreply.github.com>
2026-09-03 11:34:30 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { QueryClientProvider } from '@tanstack/react-query'
-import { WagmiProvider } from 'wagmi'
 import App from './ui/App'
-import './ui/styles.css'
-import { queryClient, wagmiConfig } from './web3'
+import { WagmiProvider } from 'wagmi'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { wagmiConfig, queryClient } from './web3'
+import '@rainbow-me/rainbowkit/styles.css'
+import { RainbowKitProvider, darkTheme } from '@rainbow-me/rainbowkit'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <RainbowKitProvider theme={darkTheme()}>
+          <App />
+        </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
   </React.StrictMode>,
